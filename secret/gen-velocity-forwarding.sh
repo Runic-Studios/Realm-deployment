efeb0a8885d198f0b26b6a7e5eb08e885402cc35
Excel619 <excel8392@gmail.com>
2025-04-15 16:35:27 -0600

    Updated config stack overlays, added velocity forwarding sec

---
+++ secret/gen-velocity-forwarding.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -euo pipefail
+
+# Assumes you have forwarding.secret in this dir with a plaintext password
+
+# Check if forwarding.secret file exists
+if [[ ! -f forwarding.secret ]]; then
+  echo "Missing forwarding.secret file"
+  exit 1
+fi
+
+kubectl create secret generic velocity-forwarding-secret \
+  --namespace placeholder \
+  --from-file=forwarding.secret=forwarding.secret \
+  --dry-run=client \
+  -o json > velocity.json
+
+kubeseal \
+  --format yaml \
+  --cert pub-cert.pem \
+  --controller-name=sealed-secrets \
+  --controller-namespace=sealed-secrets \
+  < velocity.json > forwarding-secret-sealed.yaml
+
+rm velocity.json
+
+sed -i 's/^\(\s*\)namespace: placeholder/\1namespace: {{ .Values.namespace }}/' forwarding-secret-sealed.yaml
+
+mv forwarding-secret-sealed.yaml ../templates/velocity
+
+echo "Generated templates/velocity/forwarding-secret-sealed.yaml"
